e5439593f04b294157a74f28bea85cd1ebc076e0
WonJunKim <kimjnsjwj@gmail.com>
2018-05-23 19:28:37 +0900

    Repo Init 3

--- README.md
+++ README.md
@@ -7,6 +7,14 @@
 - JWT Token
 - PassPort(?)
 
+## Modules
+- Morgan
+- Mocha
+- Express
+- Mongoose
+- Passport(Not Sure)
+- Swagger(Expected Specific Module Name)
+
 ## TODO
 - Swagger 쓰는법 알아보기
 - 가능하면 Assert, Cluster 모듈 이용해보기
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+const PORT = process.env.PORT || 12345;
+
+express()
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended : true}))
+    .get('/', (req, res) => res.send("<h1>Hello, World!</h1>"))
+    .listen(PORT, () => console.log(`Listening at ${PORT}`));
