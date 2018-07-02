7d0f37a754861a99a4af1c5382f558fb1ac1ae16
WonJunKim <kimjnsjwj@gmail.com>
2018-07-02 23:42:44 +0900

    update test cases & update redirect path

--- routes/docs/docs.spec.js
+++ routes/docs/docs.spec.js
@@ -1,13 +1,47 @@
 const supertest = require('supertest');
 const app = require('../../index');
 
-describe('/docs', () => {
+/* api/docs Test */
+describe('/api/docs', () => {
     describe('GET /', () => {
-        it('Print Hello', done => {
-            supertest(app)
-                .get('/')
+        describe('Success', () => {
+            it('Opening docs with correct key', done => {
+                supertest(app)
+                    .get('/api/docs?key=' + process.env.docsKey)
+                    .expect(301)
+                    .expect('Content-Type', /html/)
+                    .end(() => done());
+            });
+        });
+
+        describe('Failure', () => {
+            it('Opening docs with wrong key', done => {
+                supertest(app)
+                    .get('/api/docs')
+                    .expect(403)
+                    .end(() => done());
+            });
+        });
+    });
+
+    describe('GET /api-spec.json', () => {
+        describe('Success', () => {
+            it('Get api-spec.json with correct key', done => {
+                supertest(app)
+                .get('/api/docs/api-spec.json?key=' + process.env.docsKey)
                 .expect(200)
+                .expect('Content-Type', /json/)
                 .end(() => done());
+            });
+        });
+
+        describe('Failure', () => {
+            it('Get api-spec.json with wrong key', done => {
+                supertest(app)
+                .get('/api/docs/api-spec.json')
+                .expect(403)
+                .end(() => done());
+            });
         });
     });
 });
--- routes/docs/index.js
+++ routes/docs/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 
-router.get('/', (req, res) => res.redirect('/docs/swagger-ui?url=/docs/api-spec.json'));
+router.get('/', (req, res) => res.redirect('/api/docs/swagger-ui?url=/api/docs/api-spec.json'));
 router.use('/swagger-ui', express.static(pathToSwaggerUi));
 router.get('/api-spec.json', (req, res) => res.json(require('./api-spec.json')));
 
