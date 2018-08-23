831c16472e9ab2cf830fe1ccf49070cb1ee81abb
WonJunKim <kimjnsjwj@gmail.com>
2018-08-23 15:54:30 +0900

    update book model

--- model/book.js
+++ model/book.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Book = new Schema({
-    
+    title: String,
+    owner: {type: String, ref: 'users'},
+    achievementRate: Number,
+    completeDate: Date,
+    content: String
 });
 
 module.exports = mongoose.model('Book', Book);
