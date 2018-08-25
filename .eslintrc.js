fb54af8b21855f41cab98e71c8a6a2e24ee01e0d
WonJunKim <kimjnsjwj@gmail.com>
2018-08-25 11:52:04 +0900

    enable to es2017

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
         "es6": true,
         "node": true
     },
+    "parserOptions": {
+        "ecmaVersion": 2017,
+        "sourceType": "module"
+    },
     "extends": "eslint:recommended",
     "rules": {
         "no-console": "off",
