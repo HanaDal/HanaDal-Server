fd444d2f48ee67589b7c69a20ffacb690d22a3f6
WonJunKim <kimjnsjwj@gmail.com>
2018-09-03 23:29:48 +0900

    delete public/ & add api-spec.json in docs routes

---
+++ routes/challenge/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+router.route('/')
+    .get((req, res) => {
+        
+    })
+    .post((req, res) => {
+
+    })
+    .delete((req, res) => {
+        
+    });
+
+module.exports = router;
