b4446f78209f13842f506ad9efe61272379c343c
WonJunKim <kimjnsjwj@gmail.com>
2018-09-06 23:57:48 +0900

    change api-spec.json to api-spec.yaml

--- routes/docs/index.js
+++ routes/docs/index.js
@@ -5,6 +5,6 @@ const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
 
 router.get('/', (req, res) => res.redirect('/api/docs/swagger-ui?url=/api/docs/api-spec.json'));
 router.use('/swagger-ui', express.static(pathToSwaggerUi));
-router.get('/api-spec.json', (req, res) => res.json(require('../../public/api-spec.json')));
+router.get('/api-spec.yaml', express.static(__dirname + '/api-spec.yaml'));
 
 module.exports = router;
