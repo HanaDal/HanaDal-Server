d79609cbfed7fe242d183fa62e876335948a44b6
WonJunKim <kimjnsjwj@gmail.com>
2018-09-14 14:38:59 +0900

    [ADD] /api/user

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const ctrl = require('./user.ctrl');
 
-router.post('/login', ctrl.login);
-router.get('/:id', ctrl.getUserInfo);
+router.post('/login', ctrl.login)
+  .get('/profile', ctrl.getProfile)
+  .put('/profile', ctrl.modifyProfile)
+  .get('/cheering', ctrl.getCheering)
+  .post('/cheering', ctrl.cheering)
+  .get('/item', ctrl.getItems)
+  .post('/item', ctrl.buyItem)
+  .get('/store', ctrl.getStore);
 
 module.exports = router;
--- routes/user/user.ctrl.js
+++ routes/user/user.ctrl.js
@@ -1,42 +1,146 @@
 const jwt = require('jsonwebtoken');
 const User = require('../../model/user');
+const Item = require('../../model/item');
 
 // TODO: Refresh 토큰 발급 및 만료기간 정하기
-const getJWT = (id) => {
-  jwt.sign({ id }, process.env.JWT_KEY, {
-    expiresIn: '10s',
-    issuer: 'hanadal-server',
-  });
-};
+const getJWT = id => jwt.sign({ id }, process.env.JWT_KEY, { expiresIn: '30d', issuer: 'hanadal-server' });
 
-const saveNewUser = function saveNewUserAtDb(id, name, tags) {
-  const newUser = new User();
-  newUser._id = id;
-  newUser.name = name;
-  newUser.picture = 'https://www.somewhere.com';// TODO:사진추가
-  newUser.tags = tags.split(',');
-  newUser.items.skin = [];
-  newUser.items.badge = [];
-  newUser.cherring = [];
-  newUser.save();
-};
 
-// TODO: 메인에 필요한 정보 제공하기
 const login = async function login(req, res) {
-  const { id, name, tags } = req.body;
+  const {
+    id, name, tags, pictureUrl,
+  } = req.body;
   const user = await User.findOne({ id });
-  if (user) res.status(200).json(getJWT(id));
+
+  if (user) res.status(200).json({ result: 'success', jwt: getJWT(user._id) });
   else if (name !== undefined && tags !== undefined) {
-    saveNewUser(id, name, tags);
-    res.status(200).json(getJWT(id));
-  } else res.status(100).end();
+    const newUser = new User();
+    newUser.id = id;
+    newUser.name = name;
+    newUser.picture = pictureUrl || 'https://www.somewhere.com';
+    newUser.tags = tags.split(',');
+    newUser.items.skin = [];
+    newUser.items.badge = [];
+    newUser.cheering = [];
+    newUser.save()
+      .then(u => res.status(200).json({ result: 'success', jwt: getJWT(u._id) }))
+      .catch(() => res.status(500).json({ result: 'failure' }));
+  } else res.status(418).json({ result: 'continue' });
 };
 
-const getUserInfo = async function getUserInfoWithId(req, res) {
-  const user = await User.findById(req.params.id);
-  // TODO: 걸러내기
-  res.status(200).json(user);
+
+const getProfile = async function getUserInfoWithId(req, res) {
+  try {
+    const payload = jwt.verify(req.get('X-Access-Token'), process.env.JWT_KEY);
+    const user = await User.findById(payload.id);
+
+    if (!user) res.status(404).json({ result: 'failure' });
+    else {
+      res.status(200).json({
+        result: 'success',
+        name: user.name,
+        point: user.point,
+        tags: user.tags,
+      });
+    }
+  } catch (e) {
+    res.status(403).json({ result: 'failure' });
+  }
 };
+
+
+const modifyProfile = function modifyProfileWithJwt(req, res) {
+  try {
+    const { profileName, profileTags } = req.body;
+    const payload = jwt.verify(req.get('X-Access-Token'), process.env.JWT_KEY);
+    User.findByIdAndUpdate(payload.id, { name: profileName, tags: profileTags.split(',') })
+      .then(() => res.status(205).json({ result: 'success' }))
+      .catch(() => res.status(500).json({ result: 'failure' }));
+  } catch (e) {
+    res.status(403).json({ result: 'failure' });
+  }
+};
+
+
+const getCheering = function getCheering(req, res) {
+  try {
+    const payload = jwt.verify(req.get('X-Access-Token'), process.env.JWT_KEY);
+    User.findById(payload.id).populate('cheering').exec()
+      .then(u => res.status(200).json(u.cheering))
+      .catch(e => res.status(500).json({ result: 'failure', e }));
+  } catch (e) {
+    res.status(403).json({ result: 'failure' });
+  }
+};
+
+
+const cheering = function cheering(req, res) {
+  try {
+    const { id } = req.body;
+    const payload = jwt.verify(req.get('X-Access-Token'), process.env.JWT_KEY);
+    User.findByIdAndUpdate(payload.id, { $push: { cheering: id } })
+      .then(() => res.status(201).json({ result: 'success' }))
+      .catch(() => res.status(500).json({ result: 'failure' }));
+  } catch (e) {
+    res.status(403).json({ result: 'failure' });
+  }
+};
+
+
+const getItems = function getItems(req, res) {
+  try {
+    const payload = jwt.verify(req.get('X-Access-Token'), process.env.JWT_KEY);
+    User.findById(payload.id).populate('items').exec()
+      .then((u) => {
+        const skins = [];
+        const medals = [];
+        u.items.forEach(e => (e.category === 'skin' ? skins.push(e) : medals.push(e)));
+        res.status(200).json({ result: 'success', skins, medals });
+      })
+      .catch(() => res.status(500).json({ result: 'failure' }));
+  } catch (e) {
+    res.status(403).json({ result: 'failure' });
+  }
+};
+
+
+// TODO: Cost 추가, Response 403 추가, message 추가
+const buyItem = function buyItem(req, res) {
+  try {
+    const { id } = req.body;
+    const payload = jwt.verify(req.get('X-Access-Token'), process.env.JWT_KEY);
+
+    User.findOneAndUpdate({
+      _id: payload.id, items: { $nin: [id] },
+    }, { $push: { items: id } })
+      .then(async (u) => {
+        if (u === null) res.status(403).json({ result: 'failure', message: '이미 보유중인 아이템입니다' });
+        /* else if (u.point < (await Item.findById(id)).cost)
+          res.status(402).json({ result: 'failure', message: '포인트가 부족합니다' }); */
+        else res.status(200).json({ result: 'success' });
+      })
+      .catch(e => res.status(500).json({ result: 'failure', e }));
+  } catch (e) {
+    res.status(403).json({ result: 'failure' });
+  }
+};
+
+
+// TODO: Parameter에서 X-Access-Token 삭제 & Response에서 cost 추가
+const getStore = async function getStore(req, res) {
+  const items = await Item.find();
+  const skins = [];
+  const medals = [];
+  items.forEach(e => (e.category === 'skin' ? skins.push(e) : medals.push(e)));
+  res.status(200).json({ result: 'success', skins, medals });
+};
+
 
 exports.login = login;
-exports.getUserInfo = getUserInfo;
+exports.getProfile = getProfile;
+exports.modifyProfile = modifyProfile;
+exports.getCheering = getCheering;
+exports.cheering = cheering;
+exports.getItems = getItems;
+exports.buyItem = buyItem;
+exports.getStore = getStore;
