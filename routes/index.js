b8b3961873ae4c26113a7819fe7afbfb7b21bff6
WonJunKim <kimjnsjwj@gmail.com>
2018-10-05 08:27:28 +0900

    [UPDATE] add middleware

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ router.use('/user', require('./user/index'))
   .use('/challenge', require('./challenge/index'))
   .use('/qna', require('./qna/index'))
   .use('/docs', require('./docs/index'))
-  .use('/search', require('./search/index'));
+  .use('/search', require('./search/index'))
+  .use('/trending', require('./trending/index'));
 
 module.exports = router;
