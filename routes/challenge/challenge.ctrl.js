73dd4660fc6eba2dab4ccb813c77a5b52119eb1a
WonJunKim <kimjnsjwj@gmail.com>
2019-04-01 13:38:22 +0900

    [UPDATE] modify picture key

--- routes/challenge/challenge.ctrl.js
+++ routes/challenge/challenge.ctrl.js
@@ -22,7 +22,7 @@ const makeChallenge = async function makeChallenge(req, res) {
       title, description, isPublic, isStrict, tags,
     } = req.body;
     const payload = jwt.verify(req.get('X-Access-Token'), process.env.JWT_KEY);
-    const key = payload.id + title.replace(/"/g, '') + Date.now();
+    const key = Date.now() + payload.id + title.replace(/"/g, '');
     let s3Promise = Promise.resolve();
 
     const newChallenge = new Challenge({
@@ -35,7 +35,7 @@ const makeChallenge = async function makeChallenge(req, res) {
     });
 
     if (req.file) {
-      newChallenge.pictureUrl = `https://s3.amazonaws.com/hanadal-server/${key}`;
+      newChallenge.pictureUrl = `https://s3.amazonaws.com/hanadal-server/pictures/${key}`;
       s3Promise = s3.putObject({
         Bucket: 'hanadal-server',
         Key: key,
